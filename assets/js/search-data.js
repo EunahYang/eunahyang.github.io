4413d9e4dada6b2a11dfc8ebfbd1efa4be7c5ea8
EunahYang <67306771+EunahYang@users.noreply.github.com>
2025-10-12 13:52:42 +0000

    Deploying to gh-pages from @ EunahYang/eunahyang.github.io@8215f6216a8f425501818448c965af71013b32af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,12 +430,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -475,6 +470,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-pd-amyloid",
+          title: 'PD Amyloid',
+          description: "PD heterogeneity explained with Amyloid Beta",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/pd-amyloid/";
             },},{
         id: 'social-email',
         title: 'email',
