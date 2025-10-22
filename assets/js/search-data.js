e05801c386484d13a888fe435e07c77ee35e2a02
EunahYang <67306771+EunahYang@users.noreply.github.com>
2025-10-22 08:35:38 +0000

    Deploying to gh-pages from @ EunahYang/eunahyang.github.io@5b8018576475965387750a5c92a7c50213455498 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,7 +458,7 @@ ninja.data = [{
               window.location.href = "/projects/pericyte-NVJ/";
             },},{id: "projects-neuroimaging-based-transdiagnostic-approach-to-psychiatric-disorders-in-adolescents",
           title: 'Neuroimaging-based transdiagnostic approach to psychiatric disorders in adolescents',
-          description: "[&quot;2024-&quot;]",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/transdiagnostics/";
             },},{
