2a2339576688edafaf64a602e61bc3048a47c2e2
EunahYang <67306771+EunahYang@users.noreply.github.com>
2025-10-25 21:28:56 +0000

    Deploying to gh-pages from @ EunahYang/eunahyang.github.io@08886847c18f63911ad4a19090b8a8c8c66b0426 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
           description: "[&quot;2023-2025&quot;]",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pd-amyloid/";
-            },},{id: "projects-in-vivo-imaging-of-functional-hyperemia-in-arterial-stiffness-model",
-          title: 'In vivo imaging of functional hyperemia in arterial stiffness model',
-          description: "[2022]",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pericyte-NVJ/";
             },},{id: "projects-neuroimaging-based-transdiagnostic-approach-to-psychiatric-disorders-in-adolescents",
