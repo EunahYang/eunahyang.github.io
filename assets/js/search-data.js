6ab568a0ad3e9ebbfdf61f15001c42efaaa27111
EunahYang <67306771+EunahYang@users.noreply.github.com>
2025-10-27 17:40:49 +0000

    Deploying to gh-pages from @ EunahYang/eunahyang.github.io@758ae55adc7835d12504925acfbbb4a36806bea7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "What excites me keeps changing. Here is a record of my academic journey and current curiosities. Reading from the bottom section might help, since the sections are in reverse chronological order.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Research%20Interests/";
+            window.location.href = "/research-interests/";
           },
         },{id: "nav-projects",
           title: "Projects",
